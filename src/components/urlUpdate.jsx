0253968336679142c0f48334d89e3ee3169a9a63
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-05-23 11:26:20 +0200

    urlUpdate: add download feature
    In the React component UrlUpdate, a field is created to put the URL of a
    .swu file, and a download button will call a wget function in a terminal
    using the cockpit API. The downloaded file is located in
    /tmp/seapath-swu-image.swu.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

--- src/components/urlUpdate.jsx
+++ src/components/urlUpdate.jsx
@@ -4,15 +4,87 @@
  */
 
 import React from 'react';
+import cockpit from 'cockpit';
 
 class URLUpdate extends React.Component {
     constructor() {
         super();
+        this.state = {
+            url: '',
+            downloadState:'',
+            downloadLogs:'',
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleDownload = this.handleDownload.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({ url: event.target.value });
+    }
+
+    async handleDownload() {
+        const { url } = this.state;
+        const ext = this.getFileExtension(url);
+
+        if(url.trim() === ''){
+            this.setState({
+                downloadState: "failure",
+                downloadLogs: "Empty URL",
+            });
+            return;
+        }else if(ext !== "swu"){
+            this.setState({
+                downloadState: "failure",
+                downloadLogs: "SwUpdate file expected",
+            });
+            return;
+        }
+
+        this.setState({
+            downloadState: "running",
+            downloadLogs: "-",
+        });
+
+        await cockpit.spawn(["wget","-nv", "-o", "/tmp/cockpit_wget.log", url, "-O", "/tmp/seapath-swu-image.swu"])
+            .then(()=>{
+                this.setState({downloadState: "success"});
+            })
+            .catch(error=>{
+                console.log(error.message);
+                this.setState({downloadState: "failure"});
+            })
+
+        await cockpit.file("/tmp/cockpit_wget.log").read()
+            .then(downloadLogs =>{
+                this.setState({downloadLogs: downloadLogs});
+            })
+    }
+
+    getFileExtension(url) {
+        // Get the filename from the url
+        const filename = url.substring(url.lastIndexOf("/") + 1);
+
+        const extension = filename.split(".").pop();
+
+        return extension;
     }
 
     render() {
         return (
-            <div>Update with downloaded content</div>
+            <div>
+                <div className='input-container'>
+                    <input
+                        type="text"
+                        value={this.state.url}
+                        onChange={this.handleInputChange}
+                        placeholder="Enter URL"
+                    />
+                    <button onClick={this.handleDownload}>Download</button>
+                </div>
+                <div>{"Download status: " + this.state.downloadState}</div>
+                <br/>
+                <div>{"Logs: " + this.state.downloadLogs}</div>
+            </div>
         );
     }
 }
